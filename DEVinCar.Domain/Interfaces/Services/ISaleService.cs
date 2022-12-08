9ae440b08a191192e0422ec76b1fe247a8240cf8
bruna simoes moita <bruna.simoesmoita@gmail.com>
2022-12-08 17:59:12 -0300

    implements ISaleService

---
+++ DEVinCar.Domain/Interfaces/Services/ISaleService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DEVinCar.Domain.DTOs;
+
+namespace DEVinCar.Domain.Interfaces.Services
+{
+    public interface ISaleService
+    {
+        SaleCarDTO GetById(int id);
+        void InsertSale(SaleCarDTO dto);
+        void InsertDelivery(DeliveryDTO dto);
+        void UpdateAmount(int amount);
+        void UpdatePrice(decimal price);
+    }
+}
+
