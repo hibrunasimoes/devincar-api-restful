676a28922b6447f014c69581cec85d602a52e444
bruna simoes moita <bruna.simoesmoita@gmail.com>
2022-12-08 18:00:41 -0300

    implements IStateService

---
+++ DEVinCar.Domain/Interfaces/Services/IStateService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DEVinCar.Domain.DTOs;
+using DEVinCar.Domain.ViewModels;
+
+namespace DEVinCar.Domain.Interfaces.Services
+{
+    public interface IStateService
+    {
+        IList<GetStateViewModel> ListAll();
+        GetStateByIdViewModel GetStateByID(int stateId);
+        GetCityByIdViewModel GetCityById(int cityId);
+        void InsertCity(int stateId, CityDTO city);
+        void InsertAdress(int stateId, int ciryId, AddressDTO adress);
+    }
+}
+
