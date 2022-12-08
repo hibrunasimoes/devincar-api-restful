a252c8ff84cbe60702f6aadc2e670e0785e48c36
bruna simoes moita <bruna.simoesmoita@gmail.com>
2022-12-08 01:53:45 -0300

    implements interface IUserRepository

---
+++ DEVinCar.Domain/Interfaces/Repository/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DEVinCar.Domain.Models;
+
+namespace DEVinCar.Domain.Interfaces.Repository
+{
+    public interface IUserRepository
+    {
+        IList<User> ListAll();
+        User GetById(int id);
+        IList<SaleCar> GetBuyerByUserID(int id);
+        IList<SaleCar> GetSalesByUserID(int id);
+        void Insert(User user);
+        void InsertSale(Sale sale);
+        void InsertBuy(Sale buy);
+        void DeleteUser(User user);
+    }
+}
+
