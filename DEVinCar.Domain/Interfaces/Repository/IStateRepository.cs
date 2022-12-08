4cb382bf9d83d2011cf0d8797a877c3caefc6354
bruna simoes moita <bruna.simoesmoita@gmail.com>
2022-12-08 01:52:06 -0300

    implements interface IStateRepository

---
+++ DEVinCar.Domain/Interfaces/Repository/IStateRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DEVinCar.Domain.Models;
+
+namespace DEVinCar.Domain.Interfaces.Repository
+{
+    public interface IStateRepository
+    {
+        IList<State> ListAll();
+        State GetStateByID(int stateId);
+        City GetCityById(int cityId);
+        void InsertCity(City city);
+        void InsertAdress(Address adress);
+    }
+}
