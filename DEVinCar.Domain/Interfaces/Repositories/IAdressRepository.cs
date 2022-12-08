734d8a40a40555aa2ececa6ba15a5c69ab261316
bruna simoes moita <bruna.simoesmoita@gmail.com>
2022-12-08 01:43:26 -0300

    add interface-repository folders and implements IAdressRepository

---
+++ DEVinCar.Domain/Interfaces/Repositories/IAdressRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DEVinCar.Domain.Models;
+
+namespace DEVinCar.Domain.Interfaces.Repository
+{
+    public interface IAdressRepository
+    {
+        IList<Address> ListAll();
+        void Update(Address address);
+        void Delete(Address adress);
+    }
+}
+
