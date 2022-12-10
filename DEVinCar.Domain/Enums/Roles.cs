f95009aaabf8c7e102a33558a28781a645dc1023
bruna simoes moita <bruna.simoesmoita@gmail.com>
2022-12-10 17:18:46 -0300

    implement enums role

---
+++ DEVinCar.Domain/Enums/Roles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace DEVinCar.Domain.Enums
+{
+    public enum Roles
+    {
+        [Display(Name = "Manager")]
+        Manager,
+        [Display(Name = "Seller")]
+        Seller,
+        [Display(Name = "Buyer")]
+        Buyer
+    }
+}
